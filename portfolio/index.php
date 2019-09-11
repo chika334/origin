bc6f52d3263db023d6fb605833bd3c11fcc480cb
freshaluck <freshaluck@localhost.localdomain>
2019-09-11 17:48:36 -0400

    my portfolio site.

---
+++ portfolio/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script type="text/javascript" src="icons/all.js"></script>
+    <link rel="stylesheet" type="text/css" href="fontawesome.min.css">
+    <script type="text/javascript" src="js/brands.min.js"></script>
+	<title>Freshaluck</title>
+</head>
+<body>
+	<!-- start navbar -->
+	<section id="header">
+		<div class="wrapper">
+			<div class="container-fluid navbar-expand-lg navbar-light">
+				<div class="navbar">
+					<div class="navbar-brand mx-5">
+						<b>Freshaluck</b>
+					</div>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#speech">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="speech">
+						<ul class="navbar-nav ml-auto text-right">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#smartlink">Contact Us</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#about">About</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div> 
+		</div>
+	</section>
+	<!-- end navbar -->
+	<!-- start of the header image -->
+	<div class="hero-image">
+		<div class="hero-text">
+			<h1 class="text-light" style="font-size: 50px;">WEB DEVELOPER</h1>
+			<p class="font-weight-bold">
+				We build well responsive website
+			</p>
+		</div>
+	</div>
+	<!-- end of the header image -->
+	<div class="container-fluid bg-light" id="about">
+		<div class="container p-5">
+			<div class="col-12">
+				<h1>About Us...</h1>
+				<p>
+					Freshaluck is a platform for responsive and well designed website, we build all type of site from dynamic, static, ecommerce websites and more. And all these sites are built with programming with layouts designed and active with: <br>
+					<div>
+						<i class="fab fa-bootstrap fa-3x mx-5"></i>
+						<i class="fab fa-node-js fa-3x mx-5"></i>
+						<i class="fab fa-php fa-3x mx-5"></i>	
+					</div>
+				</p>
+
+				<p>
+					<b>Having the best programmers in this field your work(s) will be our best</b>
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid p-5">
+		<h1 class="text-light p-4">Some of our work</h1>
+		<div class="mx-5">
+			<img id="work" src="images/new/new.png" width="250px" height="200px">
+			<img id="work" src="images/new/screen.png" width="250px" height="200px">
+			<img id="work" src="images/new/unilag.png" width="250px" height="200px">
+		</div>
+	</div>
+
+
+	<!-- start footer -->
+	<div id="smartlink">
+		<div class="header">
+			<div class="body text-center p-5">
+				<h2>Contact</h2>
+				<a href="#"><i href="#" class="fab fa-instagram fa-3x" style="font-size: 3em; color: Tomato;"></i></a>
+				<a href="#"><i href="#" class="fab fa-twitter fa-3x" style="font-size: 3em; color: blue;"></i></a>
+				<a href="#"><i href="#" class="fab fa-facebook fa-3x" style="font-size: 3em; color: blue;"></i></a>
+				<div class="footer-copyright position-sticky text-center">
+					<p>lorem</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<!-- jquery -->
+<script type="text/javascript" src="./js/jquery-3.4.1.min.js"></script>
+<!-- bootstrap jquery -->
+<script type="text/javascript" src="./js/bootstrap.bundle.min.js"></script>
+
+</html>
